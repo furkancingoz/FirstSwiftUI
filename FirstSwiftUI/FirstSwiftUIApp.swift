7b19516660f05f17bab92aee6e8853293389e152
Furkan Cingöz <efcingoz@outlook.com>
2023-10-29 22:23:33 +0300

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Furkan Cingöz on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
