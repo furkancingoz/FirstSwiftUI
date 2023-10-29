bff0c85bbb70650863038a31e837b987fec8987c
Furkan Cingöz <efcingoz@outlook.com>
2023-10-29 23:03:50 +0300

    SwiftUI Hızlı bakış

---
+++ FirstSwiftUI/4View.swift
@@ -0,0 +1,18 @@
+//
+//  4View.swift
+//  FirstSwiftUI
+//
+//  Created by Furkan Cingöz on 29.10.2023.
+//
+
+import SwiftUI
+
+struct _View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _View()
+}
---
+++ FirstSwiftUI/ThirdView.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdView.swift
+//  FirstSwiftUI
+//
+//  Created by Furkan Cingöz on 29.10.2023.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThirdView()
+}
